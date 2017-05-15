def6f31307d1dabc1b39ad984733f5bf8ed0ba12
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-05-15 22:36:21 +0800

    Rename Config Variables

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
-var configScripts = require('../config').scripts;
+var scriptsConfig = require('../config').scripts;
 var changed = require('gulp-changed');
 var browserSync = require('browser-sync');
 
 gulp.task('js', function () {
-  gulp.src(configScripts.src)
-    .pipe(changed(configScripts.dest))
-    .pipe(gulp.dest(configScripts.dest))
+  gulp.src(scriptsConfig.src)
+    .pipe(changed(scriptsConfig.dest))
+    .pipe(gulp.dest(scriptsConfig.dest))
     .pipe(browserSync.reload({
       stream: true
     }));
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var configStyles  = require('../config').styles;
+var stylesConfig  = require('../config').styles;
 var sass = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
@@ -7,13 +7,13 @@ var browserSync  = require('browser-sync');
 var debug = require('gulp-debug');
 
 gulp.task('styles', function () {
-    return gulp.src(configStyles.src)
+    return gulp.src(stylesConfig.src)
         .pipe(sourcemaps.init())
-        .pipe(sass(configStyles.settings))
+        .pipe(sass(stylesConfigs.settings))
         .pipe(debug({title: 'styles:'}))
         .pipe(sourcemaps.write())
         .pipe(autoprefixer("last 4 versions", "> 1%"))
-        .pipe(gulp.dest(configStyles.dest))
+        .pipe(gulp.dest(stylesConfig.dest))
         .pipe(browserSync.reload({
             stream:true
         }));
