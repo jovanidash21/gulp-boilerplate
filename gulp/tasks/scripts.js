dad7185dd7c5e059c0280beb796cd5373a6e5a3a
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-05-16 21:21:50 +0800

    Update Scripts Task

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var scriptsConfig = require('../config').scripts;
-var changed = require('gulp-changed');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
 
-gulp.task('js', function () {
-  gulp.src(scriptsConfig.src)
-    .pipe(changed(scriptsConfig.dest))
+gulp.task('scripts', function() {
+  return gulp.src(scriptsConfig.src)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(scriptsConfig.dest))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
     .pipe(gulp.dest(scriptsConfig.dest))
     .pipe(browserSync.reload({
       stream: true
