a30d6185c277c8a666d259282f1fc48e5d444f46
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-05-17 21:00:43 +0800

    Update Styles Task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,20 +3,24 @@ var sass = require('gulp-sass');
 var stylesConfig  = require('../config').styles;
 var handleErrors = require('../util/handleErrors');
 var sourcemaps   = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+var rename = require("gulp-rename");
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync  = require('browser-sync');
 var debug = require('gulp-debug');
 
 gulp.task('styles', function () {
-    return gulp.src(stylesConfig.src)
-        .pipe(sass(stylesConfig.settings))
-        .on('error', handleErrors)
-        .pipe(sourcemaps.init())
-        .pipe(debug({title: 'styles:'}))
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer("last 4 versions", "> 1%"))
-        .pipe(gulp.dest(stylesConfig.dest))
-        .pipe(browserSync.reload({
-            stream:true
-        }));
+  return gulp.src(stylesConfig.src)
+    .pipe(sass(stylesConfig.settings))
+    .on('error', handleErrors)
+    .pipe(debug({title: 'styles:'}))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(cssnano())
+    .pipe(rename(stylesConfig.dest + '.css.min'))
+    .pipe(autoprefixer("last 4 versions", "> 1%"))
+    .pipe(gulp.dest(stylesConfig.dest))
+    .pipe(browserSync.reload({
+        stream:true
+    }));
 });
