7d539320f7e55ad662e293d992c61972d3d54e4f
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-05-20 20:14:49 +0800

    Update Gulp Config

--- gulp/config.js
+++ gulp/config.js
@@ -1,11 +1,17 @@
-var assets = "./assets";
-var dist = "./dist";
+var manifest = require('asset-builder')('./assets/manifest.json');
+var path = manifest.paths;
+var assets = path.source;
+var dist = path.dist;
+var config = manifest.config || {};
+var globs = manifest.globs;
+var project = manifest.getProjectGlobs();
+var revManifest = dist + 'assets.json';
 
 module.exports = {
     styles: {
-        src: assets + "/styles/**/*.{sass,scss}",
-        dest: dist + "/styles",
-        settings: {}
+        proj: project.css,
+        src: assets + 'styles',
+        dest: dist + 'styles'
     },
     scripts: {
         src: assets + "/scripts/**/*.js",
