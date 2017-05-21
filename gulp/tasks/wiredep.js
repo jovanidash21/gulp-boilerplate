4bbdacfc65f052a4df7aedd69e77a788fb36755d
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-05-21 09:56:03 +0800

    Update Wiredep Task

--- gulp/tasks/wiredep.js
+++ gulp/tasks/wiredep.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var stylesConfig  = require('../config').styles;
+var config  = require('../config');
 var wiredep = require('wiredep').stream;
 
 gulp.task('wiredep', function() {
-  return gulp.src(stylesConfig.proj)
+  return gulp.src(config.styles.proj)
     .pipe(wiredep())
     .pipe(plugins.debug({title: 'wiredep:'}))
-    .pipe(plugins.changed(stylesConfig.src, {
+    .pipe(plugins.changed(config.styles.src, {
         hasChanged: plugins.changed.compareContents
     }))
-    .pipe(gulp.dest(stylesConfig.src));
+    .pipe(gulp.dest(config.styles.src));
 });
