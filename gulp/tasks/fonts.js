2f2481f5902e6d887323ad576eef4fee60328311
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-05-31 09:42:27 +0800

    Update Fonts And Images Task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({camelize: true});
 var config = require('../config');
 var handleErrors = require('../util/handleErrors');
+var browserSync = require('browser-sync').create();
 
 gulp.task('fonts', function() {
     return gulp.src(config.fonts.globs)
@@ -9,4 +10,5 @@ gulp.task('fonts', function() {
         .on('error', handleErrors)
         .pipe(plugins.debug({title: 'fonts:'}))
         .pipe(gulp.dest(config.fonts.dest));
+        .pipe(browserSync.stream());
 });
--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({camelize: true});
 var config = require('../config');
 var handleErrors = require('../util/handleErrors');
+var browserSync = require('browser-sync').create();
 
 gulp.task('images', function() {
     return gulp.src(config.images.globs)
@@ -9,4 +10,5 @@ gulp.task('images', function() {
         .on('error', handleErrors)
         .pipe(plugins.debug({title: 'images:'}))
         .pipe(gulp.dest(config.images.dest));
+        .pipe(browserSync.stream());
 });
