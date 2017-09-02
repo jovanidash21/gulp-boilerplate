caa3bc21ad88b5d27c914efc6d837c5f20817c30
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-09-02 13:10:38 +0800

    Update Gulp Views Task

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -3,7 +3,7 @@ var plugins = require('gulp-load-plugins')({camelize: true});
 var config = require('../config');
 var handleErrors = require('../util/handleErrors');
 
-gulp.task('views', function buildHTML() {
+gulp.task('views', function() {
   return gulp.src(config.views.src)
     .pipe(plugins.pug(plugins.if(!config.enabled.production, {pretty: true})))
     .on('error', handleErrors)
